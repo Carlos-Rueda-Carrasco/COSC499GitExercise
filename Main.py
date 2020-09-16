a477b33880dbb63ad6446de9ccb0953c67731ba7
Carlos <[carlos.rueda.carrasco@gmail.com]>
2020-09-15 23:43:19 -0700

    More edits

---
+++ Main.py
@@ -0,0 +1,15 @@
+arrNum = [7, 9, 3, 5, 2, 6, 5, 4, 6, 1, 6, 3, 7, 5, 8, 3, 1, 2, 8, 9]
+arrOdd = []
+arrEven = []
+print("Welcome, to the 499 Git Exercise!")
+print("We will extract the odd and even numbers and putting them into two separate arrays./n")
+print("Each array will be sorted (both the even and the odd)")
+
+for num in arrNum:
+    if num%2 == 0:
+        arrEven.append(num)
+        arrEven.sort()
+arrNum.sort()
+
+print("Original Array (sorted): ", arrNum)
+print("Even array (sorted): ", arrEven)
