40f35fc7fd13c6a475ac1b69f1af52fdadb60d15
Carlos <[carlos.rueda.carrasco@gmail.com]>
2020-09-16 00:13:30 -0700

    More edits

--- Main.py
+++ Main.py
@@ -1,44 +1,30 @@
+import unittest
+
 arrNum = [7, 9, 3, 5, 2, 6, 5, 4, 6, 1, 6, 3, 7, 5, 8, 3, 1, 2, 8, 9]
 arrOdd = []
 arrEven = []
 print("Welcome, to the 499 Git Exercise!")
-<<<<<<< HEAD
 print(" ")
 print("We will extract the odd and even numbers and putting them into two separate arrays.")
 print(" ")
 print("Each array will be sorted (both the even and the odd)")
 print(" ")
-=======
 print("We will extract the odd and even numbers and putting them into two separate arrays./n")
->>>>>>> master
 
 for num in arrNum:
-    if num%2 == 0:
+    if num % 2 == 0:
         arrEven.append(num)
-<<<<<<< HEAD
         arrEven.sort()
 
-for num in arrNum:
-    if num%2 != 0:
-        arrOdd.append(num)
-        arrOdd.sort()
-arrNum.sort()
+
+class testEven(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(arrEven, [2, 2, 4, 6, 6, 6, 8, 8])
+
+if __name__ == '__main__':
+    unittest.main()
 
 print("Original Array (sorted): ", arrNum)
 print("Even array (sorted): ", arrEven)
-print("Odd array (sorted): ", arrOdd)
 print(" ")
 print("Done!")
-=======
-    if num%2 != 0:
-        arrOdd.append(num)
-
-
-print(arrNum)
-print(arrEven)
-print(arrOdd)
-
-
-
->>>>>>> master
-
