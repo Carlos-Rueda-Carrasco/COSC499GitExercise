ac6fb3f172380807b8c9ad3e66a9af1787a4f9cc
Carlos <[carlos.rueda.carrasco@gmail.com]>
2020-09-16 00:38:05 -0700

    Merge branch 'master' of https://github.com/Carlos-Rueda-Carrasco/COSC499GitExercise into Feature2
     Conflicts:
    	Main.py
    	README.md

--- Main.py
+++ Main.py
@@ -8,15 +8,17 @@ print("We will extract the odd and even numbers and putting them into two separa
 print(" ")
 print("Each array will be sorted (both the even and the odd)")
 print(" ")
-
 print("We will extract the odd and even numbers and putting them into two separate arrays./n")
-
 
 for num in arrNum:
     if num%2 == 0:
         arrEven.append(num)
 
         arrEven.sort()
+
+class testEven(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(arrEven, [2, 2, 4, 6, 6, 6, 8, 8])
 
 for num in arrNum:
     if num%2 != 0:
@@ -28,10 +30,11 @@ class testOdd(unittest.TestCase):
     def test2(self):
         self.assertEqual(arrOdd, [1, 1, 3, 3, 3, 5, 5, 5, 7, 7, 9, 9])
 
-
 print("Original Array (sorted): ", arrNum)
 print("Even array (sorted): ", arrEven)
 print("Odd array (sorted): ", arrOdd)
 print(" ")
 print("Done!")
 
+if __name__ == '__main__':
+    unittest.main()
