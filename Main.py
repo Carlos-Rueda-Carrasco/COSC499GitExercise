692e6e9c44e716745e4fc86062135a3b5fa55638
Carlos <carlos.rueda.carrasco@gmail.com>
2020-09-15 19:43:27 -0700

    Delete Main.py

--- Main.py
+++
@@ -1,6 +0,0 @@
-
-arrNum = [7, 9, 3, 5, 2, 6, 5, 4, 6, 1]
-print("Welcome, to the 499 Git Exercise!");
-print("We will be sorting an array of 10 integers "
-      "then we will extract the odd and even numbers and putting them into two separate arrays (also sorted).");
-print()
